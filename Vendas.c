2ec66656b4c911065a0d148ecf7f85a23b8e2d10
Matheus Vitor Ferreira <matheusvitor.dev00@gmail.com>
2024-03-14 09:15:05 -0300

    Trabalho

---
+++ Vendas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void titulo() {
+    printf("|-----------------------------------|\n");
+    printf("|          Programa de Vendas      |\n");
+    printf("|         Autores:                 |\n");
+    printf("|----------------------------------|\n");
+    printf("| Carolina de Jesus Menezes        |\n");
+    printf("| Leonardo Henrique Santos Zonta   |\n");
+    printf("| Iris da Silva Reis               |\n");
+    printf("| Matheus Vitor Ferreira           |\n");
+    printf("| Lucas Suares da Silva            |\n");
+    printf("|-----------------------------------|\n");
+}
+
+int main() {
+    titulo();
+    return 0;
+}
